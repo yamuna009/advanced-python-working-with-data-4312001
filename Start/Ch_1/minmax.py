2aaf764cb74bdd4b139c8fc2dcdd1bc394fcaf76
yamuna009 <yamuna.dsr@gmail.com>
2024-05-13 16:55:44 +0530

    Modified print

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,14 +9,25 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-
-
+print("The minimu value :",min(values))
+print("The minimu value :",min(strings))
 # TODO: The max() function finds the maximum value
-
-
+print("The max value :",max(values))
+print("The max value :",max(strings))
 # TODO: define a custom "key" function to extract a data field
-
-
+print("The minimu value :",min(strings, key=len))
+print("The max value :",max(strings, key=len))
 # TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+print(data["metadata"]["title"])
+print(len(data["features"]))
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
+
--- Start/Ch_1/utility.py
+++ Start/Ch_1/utility.py
@@ -3,27 +3,37 @@
 import json
 
 values = [0, 1, 2, 3, 4, 5]
-
+value =[1,2,3,4,5,6]
 # TODO: any() can be used to see if any value in a sequence is True
-
+print("1:",any(values))
+print("2:",any(value))
+if any(num == 3 for num in values):
+    print("3:",any(values))
 
 # TODO: all() will detect if all of the values in a sequence are True
-
-
+print("4:",all(values))
+print("5:",all(value))
+if all(values):
+    print("6:",all(values))
 # TODO: sum() can be use to add all of the values in a sequence
+print("7:",sum(values))
+
 
 
 # these utility functions don't have callbacks like min or max,
 # but we can use a generator for more fine control
 
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
 
 # TODO: are there any quake reports that were felt by more than 25,000 people?
+print("Generator function output:",any(quake["properties"]["felt"] is not None and quake["properties"]["felt"] > 25000 for quake in data["features"]))
 
+#this above line will work as generator.
 
 # TODO: how many quakes were felt by more than 500 people?
-
+print("Generator function output:",sum(quake["properties"]["felt"] is not None and quake["properties"]["felt"] >= 500 for quake in data["features"]))
 
 # TODO: how many quakes had a magnitude of 6 or larger?
+print("Generator function output:",sum(quake["properties"]["mag"] is not None and quake["properties"]["mag"] >= 6 for quake in data["features"]))
